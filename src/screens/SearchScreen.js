c5c13d4a3a7e3d800e4b95c6d692d477b7987146
Kelvin Brito <britok30@gmail.com>
2020-08-17 03:31:35 -0400

    error handling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,17 +6,22 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [results, setResults] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const searchApi = async () => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term: searchTerm,
-                location: 'New York',
-            },
-        });
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term: searchTerm,
+                    location: 'New York',
+                },
+            });
 
-        setResults(response.data.businesses);
+            setResults(response.data.businesses);
+        } catch (err) {
+            setErrorMessage('Something went wrong');
+        }
     };
 
     return (
@@ -26,6 +31,9 @@ const SearchScreen = () => {
                 onTermChange={setSearchTerm}
                 onTermSubmit={searchApi}
             />
+            {errorMessage ? (
+                <Text style={styles.errorStyle}>{errorMessage}</Text>
+            ) : null}
             <Text style={styles.textStyle}>
                 We have found {results.length} results.
             </Text>
@@ -42,6 +50,12 @@ const styles = StyleSheet.create({
         color: '#fff',
         marginTop: 10,
         fontSize: 16,
+    },
+    errorStyle: {
+        color: '#fff',
+        marginTop: 10,
+        fontSize: 20,
+        textAlign: 'center',
     },
 });
 
